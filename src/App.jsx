7a0d9158f39fd344df2c111b2e9c308abbdecee7
CoboMaster <cobomaster1@gmail.com>
2025-06-03 13:06:50 +0200

    Feat: Revision y Deploy a Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
+import './App.css';
 import FormularioGasto from './FormularioGasto';
 import ListaGastosApp from './ListaGastos';
 
 function App() {
   return (
-    <>
-      <h1 className='hea'> Gasto Mensual</h1> 
-      <ListaGastosApp/>
-      
-    </>
-  )
+    <div className="app-container">
+      <header className="hea">
+        <h1>Gasto Mensual</h1>
+      </header>
+
+      <main>
+        <ListaGastosApp />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
